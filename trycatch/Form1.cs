3842166f73f410a91defbf44f755e611665e8122
James Gittens <rc0038640@student.riversidecollege.ac.uk>
2019-11-25 15:44:38 +0000

    code

--- trycatch/Form1.cs
+++ trycatch/Form1.cs
@@ -15,6 +15,25 @@ namespace trycatch
         public Form1()
         {
             InitializeComponent();
+            string s = "3";
+            string pi = "3.14159265";
+            int i = int.Parse(s);
+            double d = double.Parse(pi);
+            richTextBox1.AppendText(i.ToString() + "\n");
+            richTextBox1.AppendText(d.ToString() + "\n");
+            string st = "three";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + "is not an integer");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }

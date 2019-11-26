79b50b5befa799e8d1ce4c30929e331da6e3732a
James Gittens <rc0038640@student.riversidecollege.ac.uk>
2019-11-26 11:28:24 +0000

    fineshed

--- trycatch/Form1.cs
+++ trycatch/Form1.cs
@@ -28,7 +28,28 @@ namespace trycatch
             }
             catch
             {
-                MessageBox.Show(st + "is not an integer");
+                MessageBox.Show(st + " is not an integer");
+                string s2 = "3.2";
+                try
+                {
+                    int i2 = int.Parse(s2);
+                }
+                catch
+                {
+                    MessageBox.Show(s2 + " is not an integer");
+                    int i2 = 0;
+                }
+                string s3 = "pi";
+                try
+                {
+                    double d2 = double.Parse(s3);
+                }
+                catch
+                {
+                    double d2 = 0;
+                }
+
+
             }
         }
         private void Form1_Load(object sender, EventArgs e)
